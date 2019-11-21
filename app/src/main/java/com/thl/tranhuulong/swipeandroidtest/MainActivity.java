41e9a4b606ca5ab53b1f674c70e1a530e7f288dc
tranlo <tranlong.kma@gmail.com>
2019-11-21 12:16:13 +0700

    setOnClick

--- app/src/main/java/com/thl/tranhuulong/swipeandroidtest/MainActivity.java
+++ app/src/main/java/com/thl/tranhuulong/swipeandroidtest/MainActivity.java
@@ -1,14 +1,55 @@
 package com.thl.tranhuulong.swipeandroidtest;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.daimajia.swipe.SwipeLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private SwipeLayout sample1, sample2, sample3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //sample2
+
+        sample2 = (SwipeLayout) findViewById(R.id.sample2);
+        sample2.setShowMode(SwipeLayout.ShowMode.LayDown);
+        sample2.addDrag(SwipeLayout.DragEdge.Right, sample2.findViewWithTag("Bottom2"));
+//        sample2.setShowMode(SwipeLayout.ShowMode.PullOut);
+
+
+        sample2.findViewById(R.id.trash).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Trash Bin", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        sample2.findViewById(R.id.edit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "edit", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        sample2.findViewById(R.id.click).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Yo", Toast.LENGTH_SHORT).show();
+            }
+        });
+        sample2.getSurfaceView().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Click on surface", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 }
